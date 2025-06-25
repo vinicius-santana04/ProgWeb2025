fcf00863a333fd56fb7feade380604b9eba2af55
viniciusantana04 <viniciusantanagato@gmail.com>
2025-06-25 13:37:13 -0400

    Atividade ExpressTs - parte 12

--- expressts/src/index.ts
+++ expressts/src/index.ts
@@ -5,9 +5,11 @@ import { logMiddleware } from './middlewares/logs';
 import { engine } from 'express-handlebars';
 
 dotenv.config();
+
 const PORT = process.env.PORT || 3333;
 const app = express();
-app.use(router);
+
+app.use(logMiddleware('completo'));
 app.use(express.static('src/public'))
 
 app.engine("handlebars", engine({
@@ -15,11 +17,11 @@ app.engine("handlebars", engine({
   layoutsDir: `${__dirname}/views/layouts`,
   defaultLayout: 'main',
 }));
+
 app.set("view engine", "handlebars");
 app.set("views", `${__dirname}/views`);
 
-// Middleware de log (troque entre 'simples' e 'completo')
-app.use(logMiddleware('completo'));
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Express app iniciada. Acesse: http://localhost:${PORT}`);
